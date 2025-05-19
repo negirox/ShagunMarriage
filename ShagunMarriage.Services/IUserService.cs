059aa2778475ec5928ec65e3fd8a0fd35e82aae7
Negirox <mukeshsingh.negi007@gmail.com>
2025-05-19 17:53:24 +0530

    Refactor user authentication to use LoginViewModel
    Updated IUserService and UserService to use LoginViewModel for authentication, improving code organization. Modified AccountController's Login method to accept a single model for user credentials, streamlining the login process. Ensured password hashing is correctly assigned in RegisterUserAsync.

--- ShagunMarriage.Services/IUserService.cs
+++ ShagunMarriage.Services/IUserService.cs
@@ -6,7 +6,7 @@ namespace ShagunMarriage.Services
     public interface IUserService
     {
         Task RegisterUserAsync(UserViewModel user);
-        Task<UserViewModel?> AuthenticateUserAsync(string username,string password);
+        Task<UserViewModel?> AuthenticateUserAsync(LoginViewModel loginViewModel);
 
         Task<UserViewModel?> GetUserInfo(UserViewModel user);
     }
